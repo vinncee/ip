1545d65b9e3acc49dc5b33c641c418929ee24f7d
vinncee <vince6921chua@gmail.com>
2025-08-16 21:54:20 +0800

    Level-1: Add echo functionality to Itachi

--- src/main/java/Itachi.java
+++ src/main/java/Itachi.java
@@ -1,6 +1,23 @@
+
+import java.util.Scanner;
+
 public class Itachi {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
         System.out.println("Hello! I'm Itachi\nWhat can I do for you?");
-        System.out.println("Bye. Hope to see you again soon!");
+
+        while (true) {
+            String input = scanner.nextLine().trim();
+            if (input.equalsIgnoreCase("bye")) {
+                System.out.println("***************************************");
+                System.out.println("Byeeee. See u in the leaf village again!");
+                System.out.println("***************************************");
+                break;
+            } else {
+                System.out.println("***************************************");
+                System.out.println(input);
+                System.out.println("***************************************");
+            }
+        }
     }
 }
