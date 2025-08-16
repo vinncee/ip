e223c5097fbbb1e6e4a43589f1d7320e53712f67
vinncee <vince6921chua@gmail.com>
2025-08-16 21:05:24 +0800

    Level-0: Rename Duke to Itachi, add greeting and exit functionality.

---
+++ src/main/java/Itachi.java
@@ -0,0 +1,6 @@
+public class Itachi {
+    public static void main(String[] args) {
+        System.out.println("Hello! I'm Itachi\nWhat can I do for you?");
+        System.out.println("Bye. Hope to see you again soon!");
+    }
+}
