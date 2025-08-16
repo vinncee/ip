95177d4d9b0048330c9ba19eac89d6ce2d003c45
vinncee <vince6921chua@gmail.com>
2025-08-16 23:35:30 +0800

    Level-3: Implement task management in Itachi.java with add, list, mark, and unmark commands

--- src/main/java/Itachi.java
+++ src/main/java/Itachi.java
@@ -5,7 +5,7 @@ public class Itachi {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Hello! I'm Itachi\nWhat can I do for you?");
-        ArrayList<String> list = new ArrayList<>();
+        ArrayList<Task> list = new ArrayList<>();
         
         while (true) {
             String input = scanner.nextLine().trim();
@@ -17,13 +17,33 @@ public class Itachi {
 
             } else if (input.equalsIgnoreCase("list")) {
                 System.out.println("-------------------------------------");
+                System.out.println("Here are the tasks in your list:");
                 for (int i = 0; i < list.size(); i++) {
-                    System.out.println(i+1 + ". " + list.get(i));
+                    System.out.println(i+1 + ". " + list.get(i).toString());
                 }
                 System.out.println("-------------------------------------");
 
+            } else if (input.toLowerCase().startsWith("mark ")) {
+                int index = Integer.parseInt(input.split(" ")[1]) - 1;
+                Task task = list.get(index);
+                task.markAsDone();
+                System.out.println("-------------------------------------");
+                System.out.println("Nice! I've marked this task as done:");
+                System.out.println(task.toString());
+                System.out.println("-------------------------------------");
+
+            } else if (input.toLowerCase().startsWith("unmark ")) {
+                int index = Integer.parseInt(input.split(" ")[1]) - 1;
+                Task task = list.get(index);
+                task.markAsNotDone();
+                System.out.println("-------------------------------------");
+                System.out.println("Sure! I've marked this task as NOT done");
+                System.out.println(task.toString());
+                System.out.println("-------------------------------------");
+
             } else {
-                list.add(input);
+                Task task = new Task(input);
+                list.add(task);
                 System.out.println("-------------------------------------");
                 System.out.println("added: " + input);
                 System.out.println("-------------------------------------");
