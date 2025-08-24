f219c2c004b90bd03673ce54443e451a4385a35d
vince <122720915+vinncee@users.noreply.github.com>
2025-08-24 15:53:42 +0800

    add toSaveFormat() method to Task, Todo, Deadline, Event to support saving tasks to file

--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -19,6 +19,10 @@ public class Task {
         this.isDone = false;
     }
 
+    public String toSaveFormat() {
+        return (isDone ? "1" : "0") + " | "+ description;
+    }
+
     @Override
     public String toString() {
         return '[' + getStatusIcon() + "] " + this.description;
