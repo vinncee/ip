b70f6258aebff483c3635c5db2598ba564ff5a26
vince <122720915+vinncee@users.noreply.github.com>
2025-08-24 17:09:19 +0800

    Add Level-8 date parsing to Deadline and Event

--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -1,21 +1,34 @@
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 public class Event extends Task {
-    protected String from;
-    protected String to;
+    protected LocalDate from;
+    protected LocalDate to;
 
     public Event(String description, String from, String to) {
         super(description);
-        this.from = from;
-        this.to = to;
+        this.from = LocalDate.parse(from.trim());
+        this.to = LocalDate.parse(to.trim());
     }
 
     @Override
     public String toString() {
-        return "[E]" + super.toString() +  "(from:" + this.from + "to:" + this.to + ")";
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MMM d yyyy");
+        return "[E]" + super.toString() +  "(from: " + this.from.format(formatter) + " to: " + this.to.format(formatter) + ")";
     }
 
 
     @Override
     public String toSaveFormat() {
         return "E | " + super.toSaveFormat() + " | " + from + " | " + to;
-}
+    }
+
+    public LocalDate getFrom() {
+        return this.from;
+    }
+
+    public LocalDate getTo() {
+        return this.to;
+    }
 }
