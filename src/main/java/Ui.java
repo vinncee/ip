9c59f549d0feebf9f19f9eb8af07e5e871fdfdb5
vince <122720915+vinncee@users.noreply.github.com>
2025-08-26 14:46:44 +0800

    Add Ui.java to handle user interactions

---
+++ src/main/java/Ui.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Ui {
+    private Scanner scanner;
+
+    public Ui() {
+        this.scanner = new Scanner(System.in);
+    }
+
+    public void showWelcome() {
+        System.out.println("\"Hello! I'm Itachi\nWhat can I do for you?");
+    }
+
+    public void showLine() {
+        System.out.println("-------------------------------------");
+    }
+
+    public void showError(String errorMsg) {
+        System.out.println("Error: " + errorMsg);
+    }
+
+    public String readCommand() {
+        return this.scanner.nextLine().trim();
+    }
+}
