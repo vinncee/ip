725759a5bd6579fe4d788ece69bc8c928964a008
vince <122720915+vinncee@users.noreply.github.com>
2025-08-27 00:35:50 +0800

    Add javadoc into ListCommand

--- src/main/java/itachi/command/ListCommand.java
+++ src/main/java/itachi/command/ListCommand.java
@@ -1,14 +1,28 @@
 package itachi.command;
+
 import java.io.IOException;
 
 import itachi.ItachiException;
 import itachi.Storage;
 import itachi.Ui;
-import itachi.command.Command;
 import itachi.task.TaskList;
 
+/**
+ * A command that lists all tasks currently in the TaskList.
+ * <p>
+ * When executed, it prints each task with its index in the list.
+ */
 public class ListCommand extends Command {
 
+    /**
+     * Executes the list command by printing all tasks in the given TaskList.
+     *
+     * @param tasks the TaskList containing all tasks
+     * @param ui the UI instance used to display output
+     * @param storage the Storage instance used to persist tasks (not used here)
+     * @throws ItachiException if any errors related to Itachi operations occur
+     * @throws IOException if there is an I/O error while accessing storage
+     */
     @Override 
     public void execute(TaskList tasks, Ui ui, Storage storage) throws ItachiException, IOException {
         ui.showLine();
