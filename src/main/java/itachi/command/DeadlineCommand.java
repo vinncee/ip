ad80c631ccc7e4854465658fc07c27ecb177a506
vince <122720915+vinncee@users.noreply.github.com>
2025-08-27 00:11:55 +0800

    Add javadoc into DeadlineCommand

--- src/main/java/itachi/command/DeadlineCommand.java
+++ src/main/java/itachi/command/DeadlineCommand.java
@@ -1,22 +1,46 @@
 package itachi.command;
-import java.io.IOException;
 
+import java.io.IOException;
 import itachi.ItachiException;
 import itachi.Storage;
 import itachi.Ui;
-import itachi.command.Command;
 import itachi.task.Deadline;
 import itachi.task.TaskList;
 
+/**
+ * Represents a command to add a {@link Deadline} task to the task list.
+ * <p>
+ * When executed, this command creates a {@link Deadline} object with
+ * the inputted description and deadline and adds it to the {@link TaskList}.
+ * It then saves the updated list to storage and via {@link Ui}, shows
+ * messages that confirms the command is executed successfully
+ */
 public class DeadlineCommand extends Command {
     private String description;
     private String by;
 
+    /**
+     * Constructs a {@code DeadlineCommand}
+     * with the given description and deadline.
+     *
+     * @param description the description of the deadline task
+     * @param by the deadline date in string format
+     */
     public DeadlineCommand(String description, String by) {
         this.description = description;
         this.by = by;
     }
 
+    /**
+     * Executes this command: adds a {@link Deadline} to the task list,
+     * saves the tasks, and displays confirmation messages.
+     *
+     * @param tasks the {@link TaskList} where the new task will be added
+     * @param ui the {@link Ui} used to display messages to the user
+     * @param storage the {@link Storage} used to save the updated task list
+     * @throws ItachiException if the command fails due to invalid input or state
+     * @throws IOException if saving the task list fails
+     */
     @Override
     public void execute(TaskList tasks, Ui ui, Storage storage) throws ItachiException, IOException{
         Deadline deadline = new Deadline(this.description, this.by);
