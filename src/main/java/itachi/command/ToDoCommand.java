ee65cde5166a97447bd1f0d72365013eb5af574f
vince <122720915+vinncee@users.noreply.github.com>
2025-08-27 00:48:14 +0800

    Add javadoc into ToDoCommand

--- src/main/java/itachi/command/ToDoCommand.java
+++ src/main/java/itachi/command/ToDoCommand.java
@@ -1,20 +1,40 @@
 package itachi.command;
+
 import java.io.IOException;
 
 import itachi.ItachiException;
 import itachi.Storage;
 import itachi.Ui;
-import itachi.command.Command;
 import itachi.task.TaskList;
 import itachi.task.Todo;
 
+/**
+ * Represents a command to add a Todo task to the task list.
+ */
 public class ToDoCommand extends Command{
+
+    /** The description of the Todo task to be added. */
     private String description;
 
+    /**
+     * Creates a ToDoCommand with the given task description.
+     *
+     * @param description the description of the Todo task
+     */
     public ToDoCommand(String description) {
         this.description = description;
     }
 
+    /**
+     * Executes the command by creating a Todo task, adding it to the TaskList,
+     * saving the updated list to storage, and displaying confirmation via the UI.
+     *
+     * @param tasks the TaskList containing all tasks
+     * @param ui the UI instance used to display output
+     * @param storage the Storage instance used to persist tasks
+     * @throws ItachiException if any errors related to Itachi operations occur
+     * @throws IOException if there is an I/O error while accessing storage
+     */
     @Override
     public void execute(TaskList tasks, Ui ui, Storage storage) throws ItachiException, IOException {
         Todo todo = new Todo(this.description);
