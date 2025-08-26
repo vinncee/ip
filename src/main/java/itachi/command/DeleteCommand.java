a365915e26a48b3891053491d2c070eb140ef303
vince <122720915+vinncee@users.noreply.github.com>
2025-08-26 16:25:17 +0800

    Add DeleteCommand class to handle deletions of tasks

---
+++ src/main/java/itachi/command/DeleteCommand.java
@@ -0,0 +1,29 @@
+package itachi.command;
+import itachi.task.TaskList;
+import itachi.Ui;
+
+import java.io.IOException;
+
+import itachi.ItachiException;
+import itachi.Storage;
+
+public class DeleteCommand extends Command {
+    private int taskNumber;
+
+    public DeleteCommand(int taskNumber) {
+        this.taskNumber = taskNumber;
+    }
+
+    @Override
+    public void execute(TaskList tasks, Ui ui, Storage storage) throws ItachiException, IOException {
+        if (taskNumber < 1 || taskNumber > tasks.size()) {
+            ui.showLine();
+            System.out.println("No such task number");
+            ui.showLine();
+            return;
+        }
+        tasks.remove(taskNumber - 1);
+        storage.save(tasks.getTasks());
+        System.out.println("Task " + this.taskNumber + " deleted");
+    }
+}
