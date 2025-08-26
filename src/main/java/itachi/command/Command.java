5c4f32de3c5d49c07efed4677508dcfaabafaf76
vince <122720915+vinncee@users.noreply.github.com>
2025-08-26 23:48:18 +0800

    add javadoc for Command class

--- src/main/java/itachi/command/Command.java
+++ src/main/java/itachi/command/Command.java
@@ -1,4 +1,5 @@
 package itachi.command;
+
 import java.io.IOException;
 
 import itachi.ItachiException;
@@ -6,9 +7,28 @@ import itachi.Storage;
 import itachi.Ui;
 import itachi.task.TaskList;
 
+/**
+ * An abstract command that can be executed by Itachi.
+ * Each of the command (e.g., ToDoCommand, ExitCommand) will extend
+ * this class and implement the {@link #execute(TaskList, Ui, Storage)} method.
+ */
 public abstract class Command {
+
+    /**
+     * Executes the command using the given task list, user interface and storage.
+     * @param tasks the list that contains all current tasks
+     * @param ui the user interface for displaying messages
+     * @param storage the storage for reading and writing task data
+     * @throws ItachiException if the command fails due to invalid input or state
+     * @throws IOException if reading or writing to storage fails
+     */
     public abstract void execute(TaskList tasks, Ui ui, Storage storage) throws ItachiException, IOException;
 
+    /**
+     * Returns whether this command is an exit command
+     * @return false by default, overridden by
+     * ExitCommand to return true
+     */
     public boolean isExit() {
         return false;
     }
