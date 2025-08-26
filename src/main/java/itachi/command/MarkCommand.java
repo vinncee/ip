44796d8821489cbcde29d19661d2cbc48216b083
vince <122720915+vinncee@users.noreply.github.com>
2025-08-27 00:41:07 +0800

    Add javadoc into MarkCommand

--- src/main/java/itachi/command/MarkCommand.java
+++ src/main/java/itachi/command/MarkCommand.java
@@ -1,20 +1,42 @@
 package itachi.command;
+
 import java.io.IOException;
 
 import itachi.ItachiException;
 import itachi.Storage;
 import itachi.Ui;
-import itachi.command.Command;
 import itachi.task.Task;
 import itachi.task.TaskList;
 
+/**
+ * Represents a command to mark a task as done.
+ * The index inputted by user represents the index
+ * of the task in the TaskList to be marked
+ */
 public class MarkCommand extends Command{
+
+    /** The 0-based index of the task to mark as done. */
     private int index;
 
+    /**
+     * Creates a MarkCommand for a specific task index.
+     *
+     * @param index the 0-based index of the task to mark
+     */
     public MarkCommand(int index) {
         this.index = index;
     }
 
+    /**
+     * Marks the task at the given index in the TaskList as done.
+     * Updates the storage and prints confirmation via the UI.
+     *
+     * @param tasks the TaskList containing all tasks
+     * @param ui the UI instance used to display output
+     * @param storage the Storage instance used to persist tasks
+     * @throws ItachiException if any errors related to Itachi operations occur
+     * @throws IOException if there is an error while accessing storage
+     */
     @Override
     public void execute(TaskList tasks, Ui ui, Storage storage) throws ItachiException, IOException {
         if (index < 0 || index >= tasks.size()) {
