3c8c7d3e66ae426bcf4a47b9e04a85084275cf91
vince <122720915+vinncee@users.noreply.github.com>
2025-08-27 00:25:53 +0800

    Add javadoc into ExitCommand

--- src/main/java/itachi/command/ExitCommand.java
+++ src/main/java/itachi/command/ExitCommand.java
@@ -4,8 +4,21 @@ import itachi.Ui;
 import itachi.command.Command;
 import itachi.task.TaskList;
 
+/**
+ * A command that exits the application.
+ * <p>
+ * When executed, it prints a farewell message to the user.
+ * The {@link #isExit()} method returns true to signal termination.
+ */
 public class ExitCommand extends Command{
 
+    /**
+     * Executes the exit command by displaying a farewell message via the Ui.
+     *
+     * @param tasks TaskList (not used in this command)
+     * @param ui Ui to display messages
+     * @param storage Storage (not used in this command)
+     */
     @Override 
     public void execute(TaskList tasks, Ui ui, Storage storage) {
         ui.showLine();
@@ -13,9 +26,13 @@ public class ExitCommand extends Command{
         ui.showLine();
     }
 
+    /**
+     * Indicates that this command will exit the application.
+     *
+     * @return true, since this command signals exit
+     */
     @Override
     public boolean isExit() {
         return true;
     }
-    
 }
