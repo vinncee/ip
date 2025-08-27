ddda9265092d76e05a5a7cfb60051db3758aae7a
vince <122720915+vinncee@users.noreply.github.com>
2025-08-27 21:26:05 +0800

    implement showFindResult method to display the matching tasks

--- src/main/java/itachi/Ui.java
+++ src/main/java/itachi/Ui.java
@@ -1,6 +1,8 @@
 package itachi;
 import java.util.Scanner;
 
+import itachi.task.TaskList;
+
 public class Ui {
     private Scanner scanner;
 
@@ -22,5 +24,19 @@ public class Ui {
 
     public String readCommand() {
         return this.scanner.nextLine().trim();
+    }
+
+    public void showFindResult(TaskList matchedTasks) {
+        if (matchedTasks.getTasks().isEmpty()) {
+            System.out.println("-------------------------------------");
+            System.out.println(" No matching tasks found.");
+            System.out.println("-------------------------------------");
+        } else {
+            System.out.println("-------------------------------------");
+            System.out.println(" Here are the matching tasks in your list:");
+            for (int i = 0; i < matchedTasks.size(); i++) {
+                System.out.println(" " + (i + 1) + ". " + matchedTasks.getTasks().get(i));
+            }
+        }
     }
 }
