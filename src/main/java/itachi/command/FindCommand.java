fb8559367ec3d79766bf67fa9172488820b158a8
vince <122720915+vinncee@users.noreply.github.com>
2025-08-28 21:33:01 +0800

    Add FindCommand class

---
+++ src/main/java/itachi/command/FindCommand.java
@@ -0,0 +1,42 @@
+package itachi.command;
+
+import itachi.task.Task;
+import itachi.task.TaskList;
+import itachi.Ui;
+import itachi.Storage;
+
+import java.util.ArrayList;
+
+public class FindCommand extends Command {
+    private final String keyword;
+
+    public FindCommand(String keyword) {
+        this.keyword = keyword;
+    }
+
+    @Override
+    public void execute(TaskList tasks, Ui ui, Storage storage) {
+        ArrayList<Task> allTasks = tasks.getTasks();
+        ArrayList<Task> matched = new ArrayList<>();
+
+        for (Task t : allTasks) {
+            if (t.getDescription().toLowerCase().contains(keyword.toLowerCase())) {
+                matched.add(t);
+            }
+        }
+
+        if (matched.isEmpty()) {
+            System.out.println("No matching tasks found for: " + keyword);
+        } else {
+            System.out.println("Here are the matching tasks:");
+            for (int i = 0; i < matched.size(); i++) {
+                System.out.println((i + 1) + ". " + matched.get(i).toString());
+            }
+        }
+    }
+
+    @Override
+    public boolean isExit() {
+        return false;
+    }
+}
