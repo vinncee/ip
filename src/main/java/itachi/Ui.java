aa3c872a792bf47164ed3162d2b64c3c25480a86
vince <122720915+vinncee@users.noreply.github.com>
2025-08-30 20:24:14 +0800

    Update Ui.java to replace System.out.println with showMessage and store messages in a list for GUI integration

--- src/main/java/itachi/Ui.java
+++ src/main/java/itachi/Ui.java
@@ -1,26 +1,41 @@
 package itachi;
 
 import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.List;
 
 import itachi.task.TaskList;
 
 public class Ui {
     private Scanner scanner;
+    private List<String> messages;
+
 
     public Ui() {
         this.scanner = new Scanner(System.in);
+        this.messages = new ArrayList<>();
+    }
+
+
+    public void startNewCommand() {
+        messages.clear();
     }
 
     public void showWelcome() {
-        System.out.println("\"Hello! I'm Itachi\nWhat can I do for you?");
+        String msg = "\"Hello! I'm Itachi\nWhat can I do for you?";
+        //System.out.println(msg);
+        showMessage(msg);
     }
 
     public void showLine() {
-        System.out.println("-------------------------------------");
+        //System.out.println("-------------------------------------");
+        showMessage("--------------------------------------");
     }
 
     public void showError(String errorMsg) {
-        System.out.println("Error: " + errorMsg);
+        String msg = "Error: " + errorMsg;
+        System.out.println(msg);
+        messages.add(msg);
     }
 
     public String readCommand() {
@@ -30,14 +45,28 @@ public class Ui {
     public void showFindResult(TaskList matchedTasks) {
         if (matchedTasks.getTasks().isEmpty()) {
             System.out.println("-------------------------------------");
+            String msg = " No matching tasks found.";
             System.out.println(" No matching tasks found.");
+            messages.add(msg);
             System.out.println("-------------------------------------");
         } else {
             System.out.println("-------------------------------------");
             System.out.println(" Here are the matching tasks in your list:");
             for (int i = 0; i < matchedTasks.size(); i++) {
+                String msg = (i + 1) + ". " + matchedTasks.getTasks().get(i);
                 System.out.println(" " + (i + 1) + ". " + matchedTasks.getTasks().get(i));
+                messages.add(msg);
             }
         }
     }
+
+    public List<String> getMessage() {
+        return new ArrayList<>(messages);
+    }
+
+    public void showMessage(String msg) {
+        System.out.println(msg); // optional for console
+        messages.add(msg);
+    }
+
 }
